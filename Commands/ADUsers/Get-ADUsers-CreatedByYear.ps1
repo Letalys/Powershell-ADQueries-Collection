17f79ef14bce2c8d53c5227f4c153e3927cbe78a
Letalys <chris.goemaere@live.fr>
2023-03-01 20:25:36 +0100

    #

---
+++ Commands/ADUsers/Get-ADUsers-CreatedByYear.ps1
@@ -0,0 +1,32 @@
+<#
+.SYNOPSIS
+	Get all user's account created in a year
+.INPUTS
+	SearchBase : Set the Active Directory search path
+    SelectedYear : Set the Year for stats
+.NOTES
+  Version:        1.0
+  Author:         Letalys
+  Creation Date:  28/02/2023
+  Purpose/Change: Initial script development
+.LINK
+    Author : Letalys (https://github.com/Letalys)
+#>
+#requires -version 4
+#Requires -Modules ActiveDirectory
+
+Function Get-ADUsers-CreatedByYear{
+    Param($SearchBase,$SelectedYear)
+    $ADQuery = Get-ADUser -filter * -Properties whenCreated -SearchBase "$SearchBase" | 
+                Where-Object {$_.whenCreated.Year -eq $SelectedYear} |
+                Group-Object {$_.whenCreated.Month} 
+    return $ADQuery
+}
+
+Try{
+    Get-ADUsers-CreatedByYear -SearchBase "<OU=,DC=,DC=>" | Format-Table -AutoSize
+    Exit 0
+}catch{
+    Write-Error "Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+    Exit 1
+}
