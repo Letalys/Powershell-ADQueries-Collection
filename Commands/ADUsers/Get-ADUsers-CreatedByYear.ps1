7d6f08c4f1021c20c6dd2c11d214c5558b1a4807
Letalys <chris.goemaere@live.fr>
2023-03-01 20:31:14 +0100

    #

--- Commands/ADUsers/Get-ADUsers-CreatedByYear.ps1
+++ Commands/ADUsers/Get-ADUsers-CreatedByYear.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-	Get all user's account created in a year
+	Get all user's account created in a year, grouped by month
 .INPUTS
 	SearchBase : Set the Active Directory search path
     SelectedYear : Set the Year for stats
@@ -19,12 +19,12 @@ Function Get-ADUsers-CreatedByYear{
     Param($SearchBase,$SelectedYear)
     $ADQuery = Get-ADUser -filter * -Properties whenCreated -SearchBase "$SearchBase" | 
                 Where-Object {$_.whenCreated.Year -eq $SelectedYear} |
-                Group-Object {$_.whenCreated.Month} 
+                Group-Object {$_.whenCreated.Month} | Select-Object Count,Name
     return $ADQuery
 }
 
 Try{
-    Get-ADUsers-CreatedByYear -SearchBase "<OU=,DC=,DC=>" | Format-Table -AutoSize
+    Get-ADUsers-CreatedByYear -SearchBase "<OU=,DC=,DC=>" -SelectedYear "<Year>" | Format-Table -AutoSize
     Exit 0
 }catch{
     Write-Error "Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
